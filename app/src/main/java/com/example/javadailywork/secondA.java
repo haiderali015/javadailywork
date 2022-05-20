7e3d1163b5f13ecda181354522617af5ff842bda
haider ali <ha347278@gmail.com>
2022-05-20 20:46:07 +0500

    list view and alphabets added in second activity

--- app/src/main/java/com/example/javadailywork/secondA.java
+++ app/src/main/java/com/example/javadailywork/secondA.java
@@ -3,12 +3,24 @@ package com.example.javadailywork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class secondA extends AppCompatActivity {
+ ListView listView;
+ String alphabets[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+ String names []={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        listView = (ListView) findViewById(R.id.alphabetlist);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplication(), android.R.layout.simple_list_item_1,names);
+        listView.setAdapter(adapter);
+
+
+
     }
 }
