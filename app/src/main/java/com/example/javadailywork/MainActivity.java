71348914c1f6aa623c4a9b51c486c761be569d9c
haider ali <ha347278@gmail.com>
2022-05-23 00:47:59 +0500

    repository inntent added that will take it to github

--- app/src/main/java/com/example/javadailywork/MainActivity.java
+++ app/src/main/java/com/example/javadailywork/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.javadailywork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn,btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn1=findViewById(R.id.repository);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/haiderali015/javadailywork");
+                Intent i = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(i);
+
+            }
+        });
 
     }
 }
